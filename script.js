4428944617b6d929b7cdf1b78092b5ddb245bc58
yuto.eguchi <gsh05196yu@yahoo.co.jp>
2020-09-03 17:38:13 +0900

    edit

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-let result = document.getElementById("result");
+let result = document.getElementById('result');
 
-    function update(elem) {
-      result.value = elem;
-    }
+function update(del) {
+  result.value = del;
+  console.log(result.value);
+}
 
-    function edit(elem) {
-      result.value += elem.value;
-    }
-    function calc() {
-      result.value = new Function("return " + result.value)();
-    }
+function edit(num) {
+  result.value += num.value;
+  console.log(result.value);
+}
+
+function calc() {
+  result.value = new Function("return " + result.value)();
+  console.log(result.value);
+}
+
