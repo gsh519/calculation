bdc0df653d07123a2958405e1622ff918e2a6f1e
yu_to <yuto1@yuuto-no-MacBook-Pro.local>
2021-01-09 22:35:42 +0900

    fix style

--- script.js
+++ script.js
@@ -2,16 +2,13 @@ let result = document.getElementById('result');
 
 function update(del) {
   result.value = del;
-  console.log(result.value);
 }
 
 function edit(num) {
   result.value += num.value;
-  console.log(result.value);
 }
 
 function calc() {
-  result.value = new Function("return " + result.value)();
-  console.log(result.value);
+  result.value = new Function(" return " + result.value)();
 }
 
